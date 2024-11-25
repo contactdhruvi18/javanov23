1ead11fc0319ab939d9316aae5155da157a3bb2d
contactdhruvi18 <contactdhruvi18@gmail.com>
2024-11-24 23:25:59 -0600

    Add files by uploading it

---
+++ javanov23/src/main/java/com/itexps/college/javanov23/Javanov23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.college.javanov23;
+
+/**
+ *
+ * @author pd
+ */
+public class Javanov23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
